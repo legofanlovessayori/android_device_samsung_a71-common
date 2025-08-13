71270c3e5d9906562f7f72275bf7f0b7f13735d7
LuK1337 <priv.luk@gmail.com>
2025-08-13 21:56:50 +0200

    a71-common: Migrate to lineagehealth IFastCharge
    Change-Id: Ia2f1f213de2f2e5afd913e5de38712fddc386b49

--- common.mk
+++ common.mk
@@ -78,10 +78,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider-service_32.samsung \
 
-# Charger
-PRODUCT_PACKAGES += \
-    vendor.lineage.fastcharge@1.0-service.samsung \
-
 # CNE
 PRODUCT_PACKAGES += \
     CneApp.libvndfwk_detect_jni.qti_vendor_symlink \
@@ -151,6 +147,9 @@ $(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class
 $(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
 $(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
 $(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
+$(call soong_config_set,lineage_health,fast_charge_node,/sys/class/sec/switch/afc_disable)
+$(call soong_config_set,lineage_health,fast_charge_value_none,1)
+$(call soong_config_set,lineage_health,fast_charge_value_fast_charge,0)
 
 PRODUCT_PACKAGES += \
     android.hardware.health-service.samsung \
